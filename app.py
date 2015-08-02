4eb5b1c7c3175737ce551f4959e2c7162af7bdbf
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-03 02:39:58 +0530

    Adds route to upload a new photo
    This is for the case where a user uploads a new photo.
    And is independent of facebook. So, the photo doesn't have
    an associated fb_id

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import json
 
 from flask import Flask, render_template, url_for, request, make_response, Response
+from werkzeug import secure_filename
 import MySQLdb
 
 from snaps.models.user import User
 from snaps.models.photo import Photo
 from snaps.models.photolist import PhotoList
 from snaps.services.photoservice import PhotoService
+
+ALLOWED_EXTENSIONS = ['jpg', 'png', 'jpeg']
 
 app = Flask(__name__)
 
@@ -16,12 +19,17 @@ db = MySQLdb.connect("localhost","sdslabs","try123","snaps")
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
+def allowed_file(filename):
+  return '.' in filename and \
+          filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET'])
 def homepage_display():
   photo_service = PhotoService()
   # photo_service.insert_into_db({"fb_id": "66666666", "filename": "66666666.jpg", ""}, db, cursor)
   # photo_service.change_state(2, 'launched', db, cursor)
   # photo_service.toggle_like(2, 6, "aNJKJHsdakdaNAKsdhkJHdanashdLDSnlja=", db, cursor)
+  # print photo_service.fetch_list(cursor, 0)
 
   # photo = Photo()
   # photo.load_from_db(3, cursor)
@@ -49,12 +57,33 @@ def homepage_display():
 
   return render_template('landing_page.html')
 
-@app.route('/photos', methods=['GET'])
+@app.route('/photos/', methods=['GET'])
 def photo_list_fetch():
   offset = request.args.get('offset', '')
 
   return Response(json.dumps({"data": [{"photo_id":"5"}, {"photo_id": "6"}]}),
                   mimetype='application/json')
+
+@app.route('/photos/new/', methods=['POST'])
+def photo_upload():
+  user_id = int(request.form['user_id'])
+  caption = request.form['caption']
+  access_token = request.form['access_token']
+
+  photo = request.files['photo']
+
+  if photo and allowed_file(photo.filename):
+    filename = secure_filename(photo.filename)
+    print filename
+
+    photo_service = PhotoService()
+    try:
+      return Response(json.dumps(
+          photo_service.upload(photo, filename, \
+                               user_id, caption, access_token, db, cursor)
+        ), mimetype='application/json')
+    except Exception, e:
+      raise e
 
 if __name__ == '__main__':
   app.run(debug=True)
