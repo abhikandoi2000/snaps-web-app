0c96b79940699ca4bdabf68a5efb1ece14a7ec09
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-02 20:28:27 +0530

    Creates PhotoList model with `load` method
    The `load` method loads photos from the db
    based on the given offset and limit

---
+++ snaps/models/photolist.py
@@ -0,0 +1,27 @@
+import MySQLdb
+
+from photo import Photo
+
+class PhotoList:
+  def __init__(self):
+    self.photos_list = []
+
+  def load(self, offset, limit, cursor):
+    sql = "SELECT id, fb_id, filename, caption, owner_id, state, created_at \
+           FROM photos \
+           WHERE state = 'launched' LIMIT %d OFFSET %d" % \
+           (limit, offset)
+
+    try:
+      cursor.execute(sql)
+      data = cursor.fetchall()
+
+      for row in data:
+        photo = Photo()
+        photo.load_from_tuple(row)
+
+        self.photos_list.append(photo)
+
+        return self.photos_list
+    except Exception, e:
+      raise e
