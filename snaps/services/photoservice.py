0c2f9b54f87eca37d657f66df99e67f41b8f9098
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-03 00:45:03 +0530

    Implements method `toggle_like` in PhotoService class
    This method verifies the authenticity of the user
    and if authentic toggle the like state for the given
    photo

--- snaps/services/photoservice.py
+++ snaps/services/photoservice.py
@@ -1,6 +1,7 @@
 import time
 
 from ..models.photo import Photo
+from ..models.user import User
 
 class PhotoService:
   def __init__(self):
@@ -15,6 +16,31 @@ class PhotoService:
       photo.save(cursor, db)
 
       return True
+    except Exception, e:
+      raise e
+
+  def toggle_like(self, photo_id, user_id, access_token, db, cursor):
+    photo = Photo()
+    user = User()
+
+    user.load_from_db(user_id, cursor)
+
+    if not user.verify_creds(access_token, cursor, db):
+      return {"error": {"message": "Authencitity of the user failed"}, "action_status": False}
+
+    # load the data for the photo
+    photo.load_from_db(photo_id, cursor)
+
+    # toggle the like on the photo
+    try:
+      if photo.check_like(user_id, db, cursor):
+        photo.unlike(user_id, db, cursor)
+
+        return {"action_performed": "unliked", "action_status": True}
+      else:
+        photo.like({"user_id": user_id, "created_at": int(time.time())}, db, cursor)
+
+        return {"action_performed": "liked", "action_status": True}
     except Exception, e:
       raise e
 
