14b89ed9cd5c3976131c6ad86f4be444a5da6439
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-02 15:51:26 +0530

    Adds placeholder template for landing page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def homepage_display():
-  return 'Authentic experiences of people you know!'
+  return render_template('landing_page.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
