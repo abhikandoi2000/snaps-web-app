96caede29b0c45511b95d52f5c98918cb676a5a2
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-02 19:20:38 +0530

    Creates Photo model with methods to load from db and tuple

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, url_for, request, make_response, Respo
 import MySQLdb
 
 from snaps.models.user import User
+from snaps.models.photo import Photo
 
 app = Flask(__name__)
 
@@ -15,6 +16,9 @@ cursor = db.cursor()
 
 @app.route('/', methods=['GET'])
 def homepage_display():
+  photo = Photo()
+  # photo.load_from_db(1, cursor)
+  photo.create((2, "2222222", "2222222.png", "But I was never meant to do this!", 5, "unreviewed", 2222222), db, cursor)
   return render_template('landing_page.html')
 
 @app.route('/photos', methods=['GET'])
---
+++ snaps/models/photo.py
@@ -0,0 +1,55 @@
+import MySQLdb
+
+class Photo:
+  def __init__(self):
+    self.data = {
+      "id": None,
+      "fb_id": None,
+      "filename": None,
+      "caption": None,
+      "owner_id": None,
+      "state": None,
+      "created_at": None
+    }
+
+  def load_from_db(self, photo_id, cursor):
+    sql = "SELECT id, fb_id, filename, caption, owner_id, state, created_at \
+           FROM photos \
+           WHERE id = %d" % photo_id
+
+    try:
+      cursor.execute(sql)
+      data = cursor.fetchone()
+
+      self.load_from_tuple(data)
+      print data
+
+      return "Hello"
+    except Exception, e:
+      raise e
+
+  def create(self, data, db, cursor):
+    sql = "INSERT INTO photos(id, fb_id, filename, caption, \
+           owner_id, state, created_at) \
+           VALUES ('%d', '%s', '%s', '%s', '%d', '%s', '%d')" % \
+           (data[0], data[1], data[2], data[3], data[4], \
+            data[5], data[6])
+
+    try:
+      cursor.execute(sql)
+
+      db.commit() # commit changes
+
+      self.load_from_tuple(data)
+    except Exception, e:
+      db.rollback()
+      raise e
+
+  def load_from_tuple(self, data):
+    self.data['id'] = data[0]
+    self.data['fb_id'] = data[1]
+    self.data['filename'] = data[2]
+    self.data['caption'] = data[3]
+    self.data['owner_id'] = data[4]
+    self.data['state'] = data[5]
+    self.data['created_at'] = data[6]
