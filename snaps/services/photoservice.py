36a7c72597a77d281c904749dd158499ff9e71c0
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-03 00:34:51 +0530

    Implements methods `insert_into_db` and `change_state` for PhotoService
    The method `change_state` will update the state of a photo
    (given its id) to the given state
    
    The method `insert_into_db` will store information for a new photo
    and return relevant instance of Photo class

--- snaps/services/photoservice.py
+++ snaps/services/photoservice.py
@@ -1,3 +1,40 @@
+import time
+
+from ..models.photo import Photo
+
 class PhotoService:
-  def __init__():
+  def __init__(self):
     pass
+
+  def change_state(self, photo_id, new_state, db, cursor):
+    photo = Photo()
+
+    try:
+      photo.load_from_db(photo_id, cursor)
+      photo.set_state(new_state)
+      photo.save(cursor, db)
+
+      return True
+    except Exception, e:
+      raise e
+
+  def insert_into_db(self, data, db, cursor):
+    photo = Photo()
+
+    try:
+      photo.create(
+        (
+          data['fb_id'],
+          data['filename'],
+          data['caption'],
+          data['owner_id'],
+          'unreviewed',
+          int(time.time())
+        )
+        , db, cursor)
+
+      return photo
+    except Exception, e:
+      raise e
+
+
