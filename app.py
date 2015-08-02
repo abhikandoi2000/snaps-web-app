b9da79d6742dc4e17e9375000a4f92a2533ed35d
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-02 16:03:03 +0530

    Added sample json response for photo list fetch route

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, url_for, request
+import json
+
+from flask import Flask, render_template, url_for, request, make_response, Response
+
+
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def homepage_display():
   return render_template('landing_page.html')
+
+@app.route('/photos', methods=['GET'])
+def photo_list_fetch():
+  offset = request.args.get('offset', '')
+
+  return Response(json.dumps({"data": [{"photo_id":"5"}, {"photo_id": "6"}]}),
+                  mimetype='application/json')
 
 if __name__ == '__main__':
   app.run(debug=True)
