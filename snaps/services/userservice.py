e31438f5631a648c4c9679a58d716a38c4cebe3c
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-03 02:54:31 +0530

    Creates class UserService to handle user creation

---
+++ snaps/services/userservice.py
@@ -0,0 +1,24 @@
+import time
+from os import path
+import json
+
+from ..models.photo import Photo
+from ..models.photolist import PhotoList
+from ..models.user import User
+
+class UserService:
+  def __init__(self):
+    pass
+
+  def create(self, name, uid, email, credentials, db, cursor):
+    user = User()
+
+    try:
+      user.create((
+          name, uid, "Facebook", json.dumps(credentials), \
+          email, int(time.time()), 0
+        ), db, cursor)
+
+      return True
+    except Exception, e:
+      raise e
