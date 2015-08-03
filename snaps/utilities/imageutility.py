55ed2595aac15a14d419d19031a31b38806e4f94
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-03 21:47:12 +0530

    Creates class ImageUtility to handle images
    Implements method `crop` to extract the square portion
    from the middle of an image using pillow

---
+++ snaps/utilities/imageutility.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+class ImageUtility:
+  def __init__(self):
+    pass
+
+  def crop(self, photo):
+    width, height = photo.size
+
+    if width > height:
+      delta = width - height
+
+      left = int(delta/2)
+      right = left + height
+
+      upper = 0
+      lower = height
+
+      photo = photo.crop((left, upper, right, lower))
+    elif height > width:
+      delta = height - width
+
+      left = 0
+      right = width
+
+      upper = int(delta/2)
+      lower = width + upper
+
+      photo = photo.crop((left, upper, right, lower))
+
+    return photo
