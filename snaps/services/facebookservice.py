0a3f99b5aad84fe527d41a1d86702ec1b6d88139
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-04 13:50:08 +0530

    Separates functionality to fetch user's fb id

--- snaps/services/facebookservice.py
+++ snaps/services/facebookservice.py
@@ -4,15 +4,20 @@ class FacebookService:
   def __init__(self, access_token):
     self.graph = facebook.GraphAPI(access_token)
 
+  def fetch_user_id(self):
+    me = self.graph.get_object(id='me')
+
+    return me['id']
+
   def fetch_all_photos(self):
     photos_list = []
 
-    me = self.graph.get_object(id='me')
+    user_id = self.fetch_user_id()
 
     photos = self.graph.get_connections(id='me', connection_name='photos', limit=500)
 
     for photo in photos['data']:
-      if photo['from']['id'] == me['id']:
+      if photo['from']['id'] == user_id:
         largest_image = photo['images'][0]
         caption = photo.get('name', '')
 
