06cde07553c9b3ba99ec670965ae4bd0b541bc26
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-10 03:42:46 +0530

    Implements method to returns photos before a timestamp
    The method `load_before` returns a list of timesorted
    (desc) list of photos before a timestamp of approval

--- snaps/models/photolist.py
+++ snaps/models/photolist.py
@@ -25,3 +25,24 @@ class PhotoList:
       return self.photos_list
     except Exception, e:
       raise e
+
+  def load_before(self, timestamp, limit, state, cursor):
+    sql = "SELECT id, fb_id, filename, caption, owner_id, state, \
+           created_at, approved_at \
+           FROM photos \
+           WHERE state = %s AND approved_at < %s \
+           ORDER BY approved_at DESC LIMIT %s"
+
+    try:
+      cursor.execute(sql, (state, timestamp, limit))
+      data = cursor.fetchall()
+
+      for row in data:
+        photo = Photo()
+        photo.load_from_tuple(row)
+
+        self.photos_list.append(photo)
+
+      return self.photos_list
+    except Exception, e:
+      raise e
